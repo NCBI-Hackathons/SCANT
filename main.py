217889f7a239978d7f799c43cc39293b63706925
Jessime <jessime.kirk@gmail.com>
2017-08-14 15:48:55 -0400

    Make run work.

--- main.py
+++ main.py
@@ -20,12 +20,10 @@ if __name__ == '__main__':
     parser.add_argument('genome', help='full path of genome file')
     parser.add_argument('sra_acc', help='SRA Run number')
     parser.add_argument('-p', '--processes', default=4, help='number of cores to use in run')
-    parser.add_argument('-o', '--outfile', default=None, help='name of file to save counts to')
+    parser.add_argument('-o', '--outfile', default='output.bam', help='name of file to save counts to')
     if len(sys.argv)==1:
         parser.print_help()
         sys.exit(0)
     args = parser.parse_args()
-    counter = BasicCounter(args.fasta, args.outfile, int(args.kmer),
-                           args.nonbinary, args.uncentered,
-                           args.unstandardized, label=args.label)
-    counter.make_count_file()
+    runner = HiSat2GFFComp(args.genome, args.sra_acc, int(args.processes), args.outfile)
+    runner.run()
