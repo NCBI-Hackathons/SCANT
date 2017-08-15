2858ed8a56e47d2ebd239014af817fa6aae5de79
Jessime <jessime.kirk@gmail.com>
2017-08-14 20:30:08 -0400

    bug fix

--- hisat.sh
+++ hisat.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-hisat2 -p $1 -x $2 --sra-acc $3 --novel_splicesite_outfile $4 | samtools view -Sb - > $5
+hisat2 -p $1 -x $2 --sra-acc $3 --novel-splicesite-outfile $4 | samtools view -Sb - > $5
