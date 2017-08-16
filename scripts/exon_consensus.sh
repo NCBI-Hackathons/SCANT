1688e008b75a1d9cc883def9f04c91b107f0ee96
alorchhota <ubuntu@ip-172-16-241-97.ec2.internal>
2017-08-16 21:18:53 +0000

    automatically detect number of columns in the bed file

--- scripts/exon_consensus.sh
+++ scripts/exon_consensus.sh
@@ -19,15 +19,20 @@ consensus_bed="$outdir/consensus.bed"
 # merge bed files and count #cells for each 
 printf  "" > $concatenated_bed
 counter=0
-for fn in "$bed_dir/"*"$bed_suffix"; 
+
+for fn in "$bed_dir/"*"$bed_suffix";
 do
+  ncol=$(awk '{FS="\t"} NR==1 {print NF}' $fn)
   counter=$((counter+1))
   awk -v counter=$counter '{print $0 "\t" counter}' $fn >> $concatenated_bed
 done
 
+
+ncol1=$((ncol+1))
 sort -k1,1 -k2,2n -k3,3n -o $sorted_bed  $concatenated_bed
 
-bedtools merge -i $sorted_bed -c 4 -o count_distinct > $consensus_bed    
+bedtools merge -i $sorted_bed -c $ncol1 -o count_distinct > $consensus_bed
+
 
 # clean files
 rm $concatenated_bed
